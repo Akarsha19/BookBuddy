7ff6f6662076541a99b360121cb7a984dbd4375b
Akarsha19 <akarshap19@gmail.com>
2024-11-21 02:47:38 -0500

    updated

--- Book-Buddy/src/SingleBook.jsx
+++ Book-Buddy/src/SingleBook.jsx
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SingleBook = () => {
+  const [selectedBook, setSelectedBook] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     const getBook = async () => {
       const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${id}`)
-      const selectedBook = await response.json();
-      console.log(selectedBook);
+      const book = await response.json();
+      console.log(book);
+      setSelectedBook(book);
     }
     getBook()
   }, []);
 
-  return (<h1>Single Book ID: {id}</h1>)
+  return (
+    <section>
+
+      <h1>{selectedBook.id}</h1>
+      <h1>{selectedBook.author}</h1>
+      <h1>{selectedBook.title}</h1>
+
+    </section >
+
+
+  )
 }
 export default SingleBook;
